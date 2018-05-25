4ed37e5f273cfbbb26fbe5dbc256dd90a2dc2fb1
liuzhao <liuzhao@ztgame.com>
2018-05-25 17:10:39 +0800

    添加简单权限处理

--- app/src/main/java/com/liuz/design/ui/LoadingActivity.java
+++ app/src/main/java/com/liuz/design/ui/LoadingActivity.java
@@ -1,12 +1,7 @@
 package com.liuz.design.ui;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
@@ -15,12 +10,8 @@ import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.liuz.design.R;
 import com.liuz.design.base.TranslucentBarBaseActivity;
-import com.liuz.design.utils.DialogUtils;
 import com.liuz.design.utils.PreferencesUtils;
 import com.liuz.lotus.loader.GlideApp;
-import com.liuz.lotus.permission.Permission;
-import com.liuz.lotus.permission.RxPermissions;
-import com.vise.utils.view.DialogUtil;
 
 import java.util.concurrent.TimeUnit;
 
@@ -52,57 +43,10 @@ public class LoadingActivity extends TranslucentBarBaseActivity {
                     }
                 });
 
-        getPermission();
+        Timer();
     }
 
-    private void getPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && mContext != null) {
-            RxPermissions rxPermissions = new RxPermissions(this);
-            rxPermissions.requestEach(Manifest.permission.WRITE_EXTERNAL_STORAGE).subscribe(new Consumer<Permission>() {
-                @Override
-                public void accept(Permission permission) throws Exception {
-                    if (permission.granted) {
-                        Timer();
-                    } else if (permission.shouldShowRequestPermissionRationale) {
-                        // 重新获取权限
-                        getPermission();
-                    } else {
-                        //提示
-                        switch (permission.name) {
-                            case Manifest.permission.WRITE_EXTERNAL_STORAGE:
-                                DialogUtils.showTips(mContext, R.string.permission_storage_title, R.string.permission_storage_des,
-                                        R.string.permission_cancel, new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                finish();
-                                            }
 
-
-                                        },
-                                        R.string.permission_ok, new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                Uri packageURI = Uri.parse("package:" + "com.liuz.design");
-                                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
-                                                startActivity(intent);
-                                                finish();
-                                            }
-
-                                        });
-                                break;
-                            default:
-                                DialogUtil.showTips(mContext, getString(R.string.permission_control),
-                                        getString(R.string.permission_noAsk) + "\n" + permission.name);
-                                break;
-                        }
-
-                    }
-                }
-            });
-        } else {
-            Timer();
-        }
-    }
 
     private void Timer() {
         Observable.timer(1, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Long>() {
--- app/src/main/java/com/liuz/design/ui/fragment/BannerItemFragment.java
+++ app/src/main/java/com/liuz/design/ui/fragment/BannerItemFragment.java
@@ -1,7 +1,12 @@
 package com.liuz.design.ui.fragment;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +15,17 @@ import android.widget.ImageView;
 
 import com.liuz.design.R;
 import com.liuz.design.ui.MainActivity;
+import com.liuz.design.utils.DialogUtils;
 import com.liuz.design.utils.PreferencesUtils;
 import com.liuz.design.view.Banners;
+import com.liuz.lotus.permission.Permission;
+import com.liuz.lotus.permission.RxPermissions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import io.reactivex.functions.Consumer;
 
 
 public class BannerItemFragment extends Fragment {
@@ -50,12 +59,58 @@ public class BannerItemFragment extends Fragment {
         }
     }
 
+    private String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
     @OnClick(R.id.iv_btn)
     void onViewClick() {
+        getPermission();
+    }
+
+    private void goMain() {
         PreferencesUtils.setFirstVisitState(false);
         startActivity(new Intent(getActivity(), MainActivity.class));
-
         getActivity().finish();
+    }
+
+    private void getPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && getActivity() != null) {
+            RxPermissions rxPermissions = new RxPermissions(getActivity());
+            rxPermissions.requestEach(permissions).subscribe(new Consumer<Permission>() {
+                @Override
+                public void accept(Permission permission) throws Exception {
+                    if (permission.granted) {
+                        goMain();
+                    } else if (permission.shouldShowRequestPermissionRationale) {
+                        // 重新获取权限
+                        getPermission();
+                    } else {
+                        //提示
+                        DialogUtils.showTips(getActivity(), R.string.permission_storage_title, R.string.permission_storage_des,
+                                R.string.permission_cancel, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        getActivity().finish();
+                                    }
+
+
+                                },
+                                R.string.permission_ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Uri packageURI = Uri.parse("package:" + "com.liuz.design");
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                                        startActivity(intent);
+                                        getActivity().finish();
+                                    }
+
+                                });
+
+
+                    }
+                }
+            });
+        } else {
+            goMain();
+        }
     }
 
     @Override
